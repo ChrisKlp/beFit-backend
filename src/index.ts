abf9ac1cd107637947391d52b2185c0d1958d65c
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2023-03-31 15:30:14 +0200

    Handle 404 status

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import rootRouter from './routes/root';
 
 dotenv.config();
@@ -13,10 +14,17 @@ app.use(express.json());
 app.use('/', express.static('public'));
 app.use('/', rootRouter);
 
-app.get('/', (req, res) => {
-  res.send('HELLO FROM EXPRESSsss');
+app.all('*', (req, res) => {
+  res.status(404);
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, '../public/404.html'));
+  } else if (req.accepts('json')) {
+    res.json({ error: 'Not found' });
+  } else {
+    res.type('txt').send('Not found');
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`now listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
