67a563d01ac42dd3efe0276c154f5771448288ea
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2023-04-01 16:26:03 +0200

    Create ingredients routes

--- src/controllers/ingredientsController.ts
+++ src/controllers/ingredientsController.ts
@@ -2,7 +2,7 @@ import asyncHandler from 'express-async-handler';
 import Ingredient from '../models/Ingredient';
 
 export const getAllIngredients = asyncHandler(async (req, res) => {
-  const ingredients = await Ingredient.find({});
+  const ingredients = await Ingredient.find().lean();
 
   if (!ingredients?.length) {
     res.status(400).json({ message: 'No ingredients found' });
@@ -13,20 +13,78 @@ export const getAllIngredients = asyncHandler(async (req, res) => {
 });
 
 export const createIngredient = asyncHandler(async (req, res) => {
-  const { name, calories, protein, carbohydrates, fat } = req.body;
+  const { name, unitWeight } = req.body;
+
+  if (!name || !unitWeight) {
+    res.status(400).json({ message: 'Missing required fields' });
+    return;
+  }
+
+  const duplicate = await Ingredient.findOne({ name }).lean().exec();
+
+  if (duplicate) {
+    res.status(409).json({ message: 'Ingredient already exists' });
+    return;
+  }
 
   const ingredient = await Ingredient.create({
     name,
-    calories,
-    protein,
-    carbohydrates,
-    fat,
+    unitWeight,
   });
 
   if (ingredient) {
-    res.status(201).json(ingredient);
+    res.status(201).json({ message: 'Ingredient created' });
   } else {
-    res.status(400);
-    throw new Error('Invalid ingredient data');
+    res.status(400).json({ message: 'Invalid ingredient data' });
   }
+});
+
+export const updateIngredient = asyncHandler(async (req, res) => {
+  const { id, name, unitWeight } = req.body;
+
+  if (!id || !name || !unitWeight) {
+    res.status(400).json({ message: 'Missing required fields' });
+    return;
+  }
+
+  const ingredient = await Ingredient.findById(id).exec();
+
+  if (!ingredient) {
+    res.status(404).json({ message: 'Ingredient not found' });
+    return;
+  }
+
+  const duplicate = await Ingredient.findOne({ name }).lean().exec();
+
+  if (duplicate && duplicate._id.toString() !== id) {
+    res.status(409).json({ message: 'Ingredient already exists' });
+    return;
+  }
+
+  ingredient.name = name;
+  ingredient.unitWeight = unitWeight;
+
+  const updatedIngredient = await ingredient.save();
+  res.json({ message: `Ingredient ${updatedIngredient.name} updated` });
+});
+
+export const deleteIngredient = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+
+  if (!id) {
+    res.status(400).json({ message: 'Missing required fields' });
+    return;
+  }
+
+  const ingredient = await Ingredient.findById(id).exec();
+
+  if (!ingredient) {
+    res.status(404).json({ message: 'Ingredient not found' });
+    return;
+  }
+
+  await ingredient.deleteOne();
+  res.json({
+    message: `Ingredient ${ingredient.name} with ${ingredient.id} deleted`,
+  });
 });
--- src/routes/ingredientRoutes.ts
+++ src/routes/ingredientRoutes.ts
@@ -2,10 +2,17 @@ import express from 'express';
 import {
   getAllIngredients,
   createIngredient,
+  updateIngredient,
+  deleteIngredient,
 } from '../controllers/ingredientsController';
 
 const router = express.Router();
 
-router.route('/').get(getAllIngredients).post(createIngredient);
+router
+  .route('/')
+  .get(getAllIngredients)
+  .post(createIngredient)
+  .patch(updateIngredient)
+  .delete(deleteIngredient);
 
 export default router;
