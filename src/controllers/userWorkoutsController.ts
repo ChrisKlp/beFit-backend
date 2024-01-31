93b833c1e817dd4058498cc257b05a3ef00ee9a0
CrisKLP <71100241+ChrisKlp@users.noreply.github.com>
2024-01-31 20:21:10 +0100

    Sort done workouts

--- src/controllers/userWorkoutsController.ts
+++ src/controllers/userWorkoutsController.ts
@@ -6,42 +6,47 @@ import getCurrentUser from '../utils/getCurrentUser';
 export const getAllUserWorkouts = async (req: Request, res: Response) => {
   const currentUser = await getCurrentUser(req);
 
-  const workouts = await UserWorkout.find({ user: currentUser._id }).populate([
-    {
-      path: 'workoutA',
-      model: 'Workout',
-      populate: {
-        path: 'exercises.exercise',
-        model: 'Exercise',
-      },
-    },
-    {
-      path: 'workoutB',
-      model: 'Workout',
-      populate: {
-        path: 'exercises.exercise',
-        model: 'Exercise',
-      },
-    },
-    {
-      path: 'workoutC',
-      model: 'Workout',
-      populate: {
-        path: 'exercises.exercise',
-        model: 'Exercise',
-      },
-    },
-    {
-      path: 'doneWorkouts',
-      model: 'UserDoneWorkout',
-      select: ['workout', 'createdAt'],
-      populate: {
-        path: 'workout',
+  const workouts = await UserWorkout.find({ user: currentUser._id })
+    .populate([
+      {
+        path: 'workoutA',
         model: 'Workout',
-        select: 'name',
+        populate: {
+          path: 'exercises.exercise',
+          model: 'Exercise',
+        },
       },
-    },
-  ]);
+      {
+        path: 'workoutB',
+        model: 'Workout',
+        populate: {
+          path: 'exercises.exercise',
+          model: 'Exercise',
+        },
+      },
+      {
+        path: 'workoutC',
+        model: 'Workout',
+        populate: {
+          path: 'exercises.exercise',
+          model: 'Exercise',
+        },
+      },
+      {
+        path: 'doneWorkouts',
+        model: 'UserDoneWorkout',
+        select: ['workout', 'createdAt'],
+        options: {
+          sort: { createdAt: -1 },
+        },
+        populate: {
+          path: 'workout',
+          model: 'Workout',
+          select: 'name',
+        },
+      },
+    ])
+    .lean();
 
   if (!workouts?.length) {
     throw new AppError('No workouts found', StatusCode.NotFound);
